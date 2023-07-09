cecb78d298871e819db1751d1b722774cf50b278
SURESHRAJPOOT <67622794+SURESHRAJPOOT@users.noreply.github.com>
2023-07-09 18:07:58 +0530

    Update main.js without path

--- main.js
+++ main.js
@@ -7,26 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const files = document.querySelectorAll('.upload-input');
     const formData = new FormData();
 
-    // Append the selected files to the formData object
-    files.forEach(function(file, index) {
-      const filePaths = [
-        '/home/suresh/Desktop/Files/frountend-demo/1ICF template Copy.docx',
-        '/home/suresh/Desktop/Files/frountend-demo/2protocol_document_input.docx',
-        '/home/suresh/Desktop/Files/frountend-demo/3Protocol_template_alternate.docx',
-        '/home/suresh/Desktop/Files/frountend-demo/4prompt_templates.csv'
-      ];
-
-      const fieldName = index === 3 ? 'prompts_file' : 'pdf' + (index + 1);
-      const fileBlob = new Blob([file.files[0]], { type: 'application/pdf' });
-      formData.append(fieldName, fileBlob, filePaths[index]);
-    });
-
     // Append the missing fields to the formData object
-    formData.append('text', pdfNameInput.value);
-    formData.append('source_template', files[0].files[0]);
-    formData.append('sink_template', files[1].files[0]);
-    formData.append('input_document', files[2].files[0]);
-    formData.append('prompts_file', files[3].files[0]);
+    formData.append('source_template', files[0].files[0]); // Assign pdf1 to source template
+    formData.append('sink_template', files[1].files[0]); // Assign pdf2 to sink template
+    formData.append('input_document', files[2].files[0]); // Assign pdf3 to input document
+    formData.append('prompts_file', files[3].files[0]); // Assign prompts file to AI training
+    formData.append('text', pdfNameInput.value); // Assign the value of the text input
 
     // Send the POST request with the updated formData object
     fetch('https://dpai-core.smarter.codes/icf/main?output_filename=res', {
